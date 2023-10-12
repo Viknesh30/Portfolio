57f166f1909861d55f2a9f488682ae515477233e
Viknesh30 <68267288+Viknesh30@users.noreply.github.com>
2023-10-13 02:09:28 +0800

    Add files via upload

---
+++ Javascript/tailwind.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+    theme: {
+      extend: {
+        colors: {
+          emerald: {
+            100: '#ECFDF5',
+            200: '#D1FAE5',
+            300: '#A7F3D0',
+            400: '#6EE7B7',
+            500: '#34D399', // Base color
+            600: '#10B981',
+            700: '#059669',
+            800: '#047857',
+            900: '#065F46',
+          },
+          teal: {
+            // Define your teal color shades here
+          },
+          indigo: {
+            // Define your indigo color shades here
+          },
+        },
+      },
+    },
+    variants: {},
+    plugins: [],
+  }
+  
